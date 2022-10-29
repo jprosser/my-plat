d031d534b87be0c751d1015070fea14bcc5299fd
Justin Prosser <jprosser@uw.edu>
2022-10-28 17:16:46 -0700

    Blank line fix for flake8.

--- my_plat/app.py
+++ my_plat/app.py
@@ -18,9 +18,11 @@ def status():
         'deployment_id': deployment_id
     }), 200 if deployment_id else 503
 
+
 @app.route("/hello")
 def hello():
     return 'Hello, World!'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
